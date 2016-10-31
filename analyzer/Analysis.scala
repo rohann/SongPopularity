a67756e99d3ea76f9624e30595d5df5c8f248ddf
rohann <rohannagrani@gmail.com>
2016-10-31 03:51:36 -0400

    adding part2 of analysis to scala file

--- analyzer/Analysis.scala
+++ analyzer/Analysis.scala
@@ -1,4 +1,5 @@
 %spark
+//PARSING
 val csvTab = "com.databricks.spark.csv"
 val csvOptionsTab = Map("mode" -> "DROPMALFORMED", "delimiter" -> "\t", "header" -> "true")
 val songA = sqlContext.read.format(csvTab).options(csvOptionsTab).load("/rohan-experiments/songOutputAHeader.txt")
@@ -6,7 +7,7 @@ val songB = sqlContext.read.format(csvTab).options(csvOptionsTab).load("/rohan-e
 val song = songA.unionAll(songB)
 song.show
 
-
+//PART 1
 import org.apache.spark.sql.Row
 val songVartist = song.select("artist_hotttnesss","hot").filter("hot>0")
 
@@ -40,3 +41,50 @@ topArtistsDF.registerTempTable("topArtists")
 
 %sql
 SELECT * FROM topArtists ORDER BY artists_hottness
+
+//PART 2
+
+
+%spark
+// getting correlation between loudness and song_hotttnesss
+val loudVHot = song.filter("hot>=0").select("loudness","hot")
+val loudVHotRDD = loudVHot.map(record=>{
+    val row2 = BigDecimal(record(1).toString.toDouble).setScale(1, BigDecimal.RoundingMode.HALF_UP)
+    (record(0).toString.toDouble,row2.toDouble)
+})
+case class loudVHotClass(loud:Double,hot: Double)
+
+
+
+
+val loudVHotDF = loudVHotRDD.map{
+    case (s0,s1) => loudVHotClass(s0,s1)
+}.toDF
+loudVHotDF.registerTempTable("loudVHot")
+
+
+%sql
+SELECT * FROM loudVHot  ORDER BY hot ASC
+
+
+//getting correlation between duration and song_hotttness
+val durationVHot = song.filter("hot>=0").select("duration","hot")
+
+
+val durationVHotRDD = durationVHot.map(record=>{
+    val row1 = BigDecimal(record(0).toString.toDouble).setScale(0, BigDecimal.RoundingMode.HALF_UP)  
+    val row2 = BigDecimal(record(1).toString.toDouble).setScale(1, BigDecimal.RoundingMode.HALF_UP)
+    (row1.toDouble,row2.toDouble)
+})
+case class DurationVHot(duration:Double,hot: Double)
+
+
+val durationVHotDF = durationVHotRDD.map{
+    case (s0,s1) => DurationVHot(s0,s1)
+}.toDF
+durationVHotDF.registerTempTable("durationVHot")
+
+
+%sql
+SELECT * FROM durationVHot ORDER BY hot ASC
+
